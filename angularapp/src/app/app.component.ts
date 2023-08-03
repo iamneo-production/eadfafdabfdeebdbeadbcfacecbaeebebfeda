6c0bc38b4b8b9de05b3417b8857bdf9c2cd77683
Iamneo <techadmin@iamneo.ai>
2023-08-03 05:03:45 +0000

    Backup Commit

--- angularapp/src/app/app.component.ts
+++ angularapp/src/app/app.component.ts
@@ -1,4 +1,11 @@
+
 import { Component } from '@angular/core';
+
+interface Food {
+  name : string;
+  calories : any;
+  image : string;
+}
 
 @Component({
   selector: 'app-root',
@@ -7,4 +14,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angularapp';
+
+  FoodList : Food[] = [] ;
+
+
+  changeValue(event : any,option : any) {
+    console.log(event.target.value);
+    if(option == "n")
+      document.getElementById("name")?.setAttribute("value",event.target.value);
+    else if(option == "c")
+      document.getElementById("calories")?.setAttribute("value",event.target.value);
+    else
+      document.getElementById("image")?.setAttribute("value",event.target.value);
+  }
+
+  addFood() {
+    const name : any= document.getElementById("name")?.getAttribute("value");
+    const calories :any = document.getElementById("calories")?.getAttribute("value");
+    const image : any = document.getElementById("image")?.getAttribute("value");
+    const food : Food = {
+      name : name,
+      calories : calories,
+      image : image
+    }
+    console.log(food);
+    this.FoodList.push(food);
+  }
 }
